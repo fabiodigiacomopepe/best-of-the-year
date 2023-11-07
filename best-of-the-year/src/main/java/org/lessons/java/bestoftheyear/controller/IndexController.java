814cfaee2c959d9134231ba6b4721edbf3d9e2d1
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-11-07 14:39:46 +0100

    Step 1: DONE

---
+++ best-of-the-year/src/main/java/org/lessons/java/bestoftheyear/controller/IndexController.java
@@ -0,0 +1,17 @@
+package org.lessons.java.bestoftheyear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping("/")
+    public String home(Model model) {
+        String myName = "Fabio";
+        model.addAttribute("name", myName);
+        return "home-page";
+    }
+}
