7eb2667d5ed9a697d2ecf561a2febf9ae54178d7
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-11-07 14:28:56 +0100

    Initial commit

---
+++ best-of-the-year/src/main/java/org/lessons/java/bestoftheyear/BestOfTheYearApplication.java
@@ -0,0 +1,14 @@
+package org.lessons.java.bestoftheyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestOfTheYearApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestOfTheYearApplication.class, args);
+    }
+
+}
